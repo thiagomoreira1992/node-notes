41c9bd14e8caf85477710dbdb52f6b4d4011f537
Thiago Moreira <thiagomoreira.ifsc@gmail.com>
2023-01-27 18:05:03 -0300

    update

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,11 @@
-const { Router } = require('express');
+const { Router, response } = require('express');
+const multer = require('multer')
+const uploadConfig = require('../configs/uploads')
+
 
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER)
 
 const UsersController = require('../controllers/UsersController')
 const ensureAuthentitcate = require("../middlewares/ensureAuthenticate")
@@ -11,5 +15,9 @@ const usersController = new UsersController();
 usersRoutes.post('/', usersController.create);
 usersRoutes.put('/', ensureAuthentitcate,usersController.update);
 usersRoutes.get('/', usersController.index)
+usersRoutes.patch('/avatar', ensureAuthentitcate, upload.single('avatar'), (req, res) => {
+    console.log(req.file.filename)
+    res.json()
+})
 
 module.exports = usersRoutes;
